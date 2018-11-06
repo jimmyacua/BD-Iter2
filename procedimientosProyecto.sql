c9f2b9df41f8a7bb16e007f9290b7743002ea6ce
Katherine Vanessa Hernandez Benavides <b63318@ecci.ucr.ac.cr>
2018-11-06 12:28:07 -0600

    script con proc almac

---
+++ procedimientosProyecto.sql
@@ -0,0 +1,88 @@
+use DB_EQUIPO_A;
+
+--drop procedure agregarAnunciante
+
+--AGREGAR ANUNCIANTE
+GO
+CREATE PROCEDURE agregarAnunciante 
+	@nombre varchar(30), 
+	@tipo varchar(15)
+AS
+INSERT INTO Usuario(Nombre)
+VALUES(@nombre);
+INSERT INTO Anunciante (NombreAn,Tipo)
+VALUES(@nombre ,@tipo);
+
+--AGREGAR INTERPRETES ORIGINALES
+GO
+CREATE PROCEDURE agregarInterprete
+	@nombre varchar(30)
+AS
+INSERT INTO InterpreteOriginal(Nombre)
+VALUES(@nombre); 
+
+--AGREGAR CANCIONES
+GO
+CREATE PROCEDURE agregarCanciones
+	@nombre varchar(30),
+	@nombreIn varchar(30),
+	@idioma varchar(30),
+	@genero varchar(30)
+AS
+INSERT INTO Cancion(Nombre, NombreInterprete, NombreIdioma, NombreGenero)
+VALUES(@nombre, @nombreIn, @idioma, @genero);
+
+-- AGREGAR TIPOS DE ENTRADA
+GO
+CREATE PROCEDURE agregarTipoDeEntrada
+	@categoria VARCHAR(30),
+	@precio FLOAT,
+	@nombreConcierto VARCHAR(30),
+	@nombreAnunciante VARCHAR(30)
+AS
+	INSERT INTO Entrada(Categoria, Precio, NombreConcierto, NombreAn)
+	VALUES(@categoria, @precio, @nombreConcierto, @nombreAnunciante);
+
+--AGREGAR LUGAR
+GO
+CREATE PROCEDURE agregarLugar
+	@nombre varchar(30),
+	@tipoLugar varchar(30),
+	@ubicacion varchar(30),
+	@canton varchar(30),
+	@provincia varchar(30)
+AS
+INSERT INTO Lugar(NombreLugar, NombreTipo, UbicacionExacta, NombreCanton, NombreProvincia)
+VALUES(@nombre, @tipoLugar, @ubicacion, @canton, @provincia)
+
+--AGREGAR CONCIERTO
+GO
+CREATE PROCEDURE agregarConcierto
+	@nombre varchar(30),
+	@nombreAn varchar(30),
+	@cupo int,
+	@descripcion varchar(50),
+	@lugar varchar(30)
+AS
+INSERT INTO Concierto(NombreConcierto, NombreAn, Cupo, Descripcion, NombreLugar)
+VALUES(@nombre, @nombreAn, @cupo, @descripcion, @lugar)
+
+
+--CONSULTAR ANUNCIANTE POR NOMBRE
+GO
+CREATE PROCEDURE consultarAnuncianteNombre
+	@nombreUsuario varchar(30)
+AS
+SELECT NombreAn
+FROM Anunciante
+WHERE NombreAn = @nombreUsuario
+
+--CONSULTAR CONCIERTOS
+GO
+CREATE PROCEDURE consultarConciertos
+AS
+SELECT *
+FROM Concierto
+
+
+ 
